f6b77c49d8d1999132d892f44207866d1d93c5b2
fahadbt <74052480+fahadbt@users.noreply.github.com>
2023-02-08 20:40:06 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,9 @@ import image1 from '../assets/technology.png'
 const Hero = () => {
   return (
     <div className='h-[100em] py-20 bg-primary justify-between items-center'>
-      <div className=''>
-        <img src={image1} alt="" className='' />
-        <img src={image1} alt="" className='' />
-      </div>
+      <div>
+    It's only Navbar
+    </div>
     </div>
   )
 }
