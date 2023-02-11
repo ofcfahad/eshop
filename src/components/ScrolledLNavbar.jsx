dc90a6f2ab3880c08083432a64c9915751d60b99
fahadbt <74052480+fahadbt@users.noreply.github.com>
2023-02-11 11:12:31 +0500

    Update ScrolledLNavbar.jsx

--- src/components/ScrolledLNavbar.jsx
+++ src/components/ScrolledLNavbar.jsx
@@ -10,7 +10,9 @@ import { RxDividerVertical } from 'react-icons/rx';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { HiOutlineBars3, HiOutlineBars3CenterLeft } from 'react-icons/hi2'
 
-const ScrolledLNavbar = () => {
+const ScrolledLNavbar = (props) => {
+
+    const theme = props.theme
 
     const [ishovered, setishovered] = useState(false)
     const [isextended, setisextended] = useState(false)
@@ -18,19 +20,19 @@ const ScrolledLNavbar = () => {
     const [showDiv, setShowDiv] = useState(true);
 
     const nHiOutlineBars3CenterLeft = (<div className=''>
-        <IconContext.Provider value={{ color: 'black', size: 18 }}>
+        <IconContext.Provider value={{ color: theme === 'bg-secondary' ? 'white' : 'black', size: 18 }}>
             <HiOutlineBars3CenterLeft />
         </IconContext.Provider>
     </div>);
 
     const nHiOutlineBars3 = (<div className=''>
-        <IconContext.Provider value={{ color: 'black', size: 18 }}>
+        <IconContext.Provider value={{ color: theme === 'bg-secondary' ? 'white' : 'black', size: 18 }}>
             <HiOutlineBars3 />
         </IconContext.Provider>
     </div>);
 
     return (
-        <nav className='w-full fixed bg-primary h-[50px] items-center border-b border-black border-opacity-10 transition-all ease-in delay-100'>
+        <nav className={`w-full fixed ${theme} h-[50px] items-center border-b border-black border-opacity-10 transition-all ease-in delay-100`}>
             <div className='flex mt-[-10px]'>
                 {/* CATEGORY */}
                 <button className='h-auto ml-[15px]' onMouseOver={() => setishovered(true)} onMouseLeave={() => setishovered(false)}>
@@ -53,14 +55,14 @@ const ScrolledLNavbar = () => {
                 {/* LOGINBUTTON */}
                 <button className='flex w-auto h-[28px] justify-center items-center px-1 rounded-lg border border-black border-opacity-5 shadow-md hover:mr-[-2px] hover:border-opacity-10 hover:shadow-lg'>
                     <div className=''>
-                        <IconContext.Provider value={{ color: 'black', size: 20 }}>
+                        <IconContext.Provider value={{ color: theme === 'bg-secondary' ? 'white' : 'black', size: 20 }}>
                             <CiLogin />
                         </IconContext.Provider>
                     </div>
                 </button>
                 {/* DIVIDER */}
                 <div className=''>
-                    <IconContext.Provider value={{ color: "gray", size: 30 }}>
+                    <IconContext.Provider value={{ color: theme === 'bg-secondary' ? 'white' : 'gray', size: 30 }}>
                         <RxDividerVertical />
                     </IconContext.Provider>
                 </div>
@@ -70,7 +72,7 @@ const ScrolledLNavbar = () => {
                 </button>
                 {/* DIVIDER */}
                 <div className=''>
-                    <IconContext.Provider value={{ color: "gray", size: 20 }}>
+                    <IconContext.Provider value={{ color: theme === 'bg-secondary' ? 'white' : 'gray', size: 20 }}>
                         <RxDividerVertical />
                     </IconContext.Provider>
                 </div>
