c789bb95f7e282852938bbb27759f420e04c14b6
fahadbt <74052480+fahadbt@users.noreply.github.com>
2023-02-11 20:44:09 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import image1 from '../assets/technology.png'
 
 const Hero = (props) => {
   return (
-    <div className={`h-[100em] py-20 ${props.theme} justify-between items-center`}>
-      <div className=''>
+    <div className={`h-[2000px] py-[400px] ${props.theme} justify-center items-center`}>
+      <div className={`text-[30px] ${props.theme === 'bg-secondary' ? `text-white` : `text-black`} text-center`}>
         It's just Navbar innit?
       </div>
     </div>
