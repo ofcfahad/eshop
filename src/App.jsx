0d8b65696cd102fe12a27ab56fc3e9710e6adbec
fahadbt <74052480+fahadbt@users.noreply.github.com>
2023-02-11 20:43:23 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Hero from './components/Hero'
 
 const App = () => {
 
-  const [theme, setTheme] = useState('bg-primary');
+  const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+  const [theme, setTheme] = useState(darkTheme.matches ? 'bg-secondary' : 'bg-primary');
 
   const toggleTheme = () => {
     setTheme(theme === 'bg-primary' ? 'bg-secondary' : 'bg-primary');
   };
 
   return (
-    <div className='bg-primary'>
+    <div className={`h-full ${theme}`}>
       <Navbar
         theme={theme}
         toggleTheme={toggleTheme}
+        setTheme={setTheme}
       />
       <Hero
         theme={theme}
