3de69468efe2ce3c352cc185bdd1dc57bc41f8a3
fahadbt <74052480+fahadbt@users.noreply.github.com>
2023-02-11 11:25:07 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 
 const App = () => {
+
+  const [theme, setTheme] = useState('bg-primary');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'bg-primary' ? 'bg-secondary' : 'bg-primary');
+  };
+
   return (
     <div className='bg-primary'>
-      <Navbar /> 
-      <Hero />
+      <Navbar
+        theme={theme}
+        toggleTheme={toggleTheme}
+      />
+      <Hero
+        theme={theme}
+      />
     </div>
   )
 }
