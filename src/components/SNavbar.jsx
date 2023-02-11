7d7f10727b38463762939a5e39122d6a389f7522
fahadbt <74052480+fahadbt@users.noreply.github.com>
2023-02-11 20:46:51 +0500

    Update SNavbar.jsx

--- src/components/SNavbar.jsx
+++ src/components/SNavbar.jsx
@@ -14,42 +14,53 @@ import Cart from './Cart';
 import { Switch } from '@mui/material';
 import Settings from './Settings';
 
-const xNavbar = () => {
+const xNavbar = (props) => {
+    const [isDark, setisDark] = useState(props.theme === 'bg-secondary' ? true : false)
 
     const [ishovered, setishovered] = useState(false)
 
     const [showDiv, setShowDiv] = useState(true);
 
     const nHiOutlineBars3CenterLeft = (<div className=''>
-        <IconContext.Provider value={{ color: 'black', size: 18 }}>
+        <IconContext.Provider value={{ color: isDark ? 'white' : 'black', size: 18 }}>
             <HiOutlineBars3CenterLeft />
         </IconContext.Provider>
     </div>);
 
     const nHiOutlineBars3 = (<div className=''>
-        <IconContext.Provider value={{ color: 'black', size: 18 }}>
+        <IconContext.Provider value={{ color: isDark ? 'white' : 'black', size: 18 }}>
             <HiOutlineBars3 />
         </IconContext.Provider>
     </div>);
 
     const sb620 = (<div className='h-[35px] ss:w-[500px] float-left mt-1.5 ml-[15px] xs:w-[300px] 600px:w-[100px] transition-all ease-linear delay-100'>
-        <SearchBar />
+        <SearchBar
+            isDark={isDark}
+        />
     </div>)
 
     const sb520 = (<div className='h-[35px] w-[400px] float-left mt-1.5 ml-[15px] transition-all ease-linear delay-10'>
-        <SearchBar />
+        <SearchBar
+            isDark={isDark}
+        />
     </div>)
 
     const sb420 = (<div className='h-[35px] w-[300px] float-left mt-1.5 ml-[15px] transition-all ease-linear delay-100'>
-        <SearchBar />
+        <SearchBar
+            isDark={isDark}
+        />
     </div>)
 
     const sb320 = (<div className='h-[35px] w-[200px] float-left mt-1.5 ml-[15px] transition-all ease-linear delay-100'>
-        <SearchBar />
+        <SearchBar
+            isDark={isDark}
+        />
     </div>)
 
     const sb220 = (<div className='h-[35px] w-[100px] float-left mt-1.5 ml-[15px] transition-all ease-linear delay-100'>
-        <SearchBar />
+        <SearchBar
+            isDark={isDark}
+        />
     </div>)
 
     const [width, setWidth] = React.useState(window.innerWidth);
@@ -62,7 +73,7 @@ const xNavbar = () => {
 
 
     return (
-        <nav className='bg-primary h-[120px] w-full'>
+        <nav className={`${props.theme} h-[120px] w-full`}>
             {/* UPPERS */}
             <div className='h-[60%] border-b border-black border-opacity-10 justify-between items-center'>
                 {/* CATEGORY */}
@@ -71,13 +82,13 @@ const xNavbar = () => {
                 </button>
                 {/* SETTINGS */}
                 <div className='flex float-right mt-[25px] mr-[15px]'>
-                    <Settings />
+                    <Settings
+                        isDark={isDark}
+                        setisDark={setisDark}
+                        theme={props.theme}
+                        setTheme={props.setTheme}
+                    />
                 </div>
-                {/*  <button className='flex float-right mt-[25px] mr-[15px]'>
-                    <IconContext.Provider value={{ color: 'black', size: 20 }}>
-                        <CiSettings />
-                    </IconContext.Provider>
-                </button> */}
                 {/* LOGO */}
                 <img src={logo} alt="logo" className='h-[100%] m-auto' />
             </div>
@@ -90,23 +101,25 @@ const xNavbar = () => {
                 {/* OTHERS */}
                 <div className='flex float-right justify-center items-center mt-1.5'>
                     {/* LOGINBUTTON */}
-                    <button className='flex w-auto h-[28px] justify-center items-center px-1 rounded-lg border border-black border-opacity-5 shadow-md hover:mr-[-2px] hover:border-opacity-10 hover:shadow-lg'>
+                    <button className={`flex w-auto h-[28px] justify-center items-center px-1 rounded-lg border ${isDark ? `border-white` : `border-black`} border-opacity-5 shadow-md hover:mr-[-2px] hover:border-opacity-10 hover:shadow-lg`}>
                         {/* ICON */}
                         <div className=''>
-                            <IconContext.Provider value={{ color: 'black', size: 20 }}>
+                            <IconContext.Provider value={{ color: isDark ? 'white' : 'black', size: 20 }}>
                                 <CiLogin />
                             </IconContext.Provider>
                         </div>
                     </button>
                     {/* DIVIDER */}
                     <div className=''>
-                        <IconContext.Provider value={{ color: "gray", size: 30 }}>
+                        <IconContext.Provider value={{ color: isDark ? 'white' : 'gray', size: 30 }}>
                             <RxDividerVertical />
                         </IconContext.Provider>
                     </div>
                     {/* CART */}
                     <button className='mr-2' onClick={() => console.log("Cart is Under Devlopment")}>
-                        <Cart />
+                        <Cart
+                            isDark={isDark}
+                        />
                     </button>
                 </div>
             </div>
