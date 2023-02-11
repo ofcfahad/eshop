e12e3c806fb4e20296df30c44db8c0d5e8df2f10
fahadbt <74052480+fahadbt@users.noreply.github.com>
2023-02-11 20:43:47 +0500

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,22 +3,22 @@ import { useState } from 'react';
 import { IconContext } from "react-icons";
 import { CiShoppingCart } from 'react-icons/ci'
 
-const Cart = () => {
+const Cart = (props) => {
   const [showDiv, setShowDiv] = useState(false);
 
   return (
     <div className="mr-[10px]" onMouseEnter={() => setShowDiv(true)} onMouseLeave={() => setShowDiv(false)}>
-      <IconContext.Provider value={{ color: "black", size: 20 }}>
+      <IconContext.Provider value={{ color: props.isDark || props.theme === 'bg-secondary' ? 'white' : 'black', size: 20 }}>
         <CiShoppingCart />
       </IconContext.Provider>
       {showDiv && (
-        <div className="hidden lg:block cursor-default bg-[rgba(255,255,255)] h-[180px] w-[250px] rounded-xl absolute right-1 top-[40px]">
+        <div className={`hidden lg:block cursor-default ${props.isDark || props.theme === 'bg-secondary' ? `bg-[#181717]` : `bg-[rgb(255,255,255)]`} h-[180px] w-[250px] rounded-xl absolute right-1 top-[40px]`}>
           <div className='divide-y'>
             <div className='mt-2 text-sm border-b border-b-white border-opacity-10'>
-              <div className='ml-3 float-left'>
+              <div className={`ml-3 float-left ${props.isDark || props.theme === 'bg-secondary' ? `text-white` : `text-black`}`}>
                 Products
               </div>
-              <button className='float-right mr-3 text-blue-500'>
+              <button className={`float-right mr-3 ${props.isDark || props.theme === 'bg-secondary' ? `text-green-600` : `text-blue-500`} `}>
                 Cart
               </button>
             </div>
@@ -26,7 +26,7 @@ const Cart = () => {
             <div>
             </div>
           </div>
-          <div className='text-center mt-[50px] text-sm justify-around items-center font-semibold'>
+          <div className={`text-center mt-[50px] text-md ${props.isDark || props.theme === 'bg-secondary' ? `text-white` : `text-black`} justify-around items-center font-bold`}>
             It's always gonna be Empty Right?
           </div>
         </div>
